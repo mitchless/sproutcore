0ca9f902f59f9d05cc5f65e5a99fb5127745e79e
Greg Fairbanks <fairbanksg@seapine.com>
2012-11-06 15:24:27 -0500

    Revert "Fix memory leak in SC.Object.extend."
    This reverts commit c8f8f5cad095d407365f6a01d3af103787dfe6b9.

--- frameworks/runtime/system/object.js
+++ frameworks/runtime/system/object.js
@@ -334,6 +334,9 @@ SC.mixin(SC.Object, /** @scope SC.Object */ {
     ret.__sc_super__ = this.prototype;
     SC.generateGuid(ret, "sc"); // setup guid
 
+    ret.subclasses = SC.Set.create();
+    this.subclasses.add(ret); // now we can walk a class hierarchy
+
     // setup new prototype and add properties to it
     var base = (ret.prototype = SC.beget(this.prototype)),
         idx, len = arguments.length;
@@ -419,6 +422,14 @@ SC.mixin(SC.Object, /** @scope SC.Object */ {
     @type Boolean
   */
   isClass: YES,
+
+  /**
+    Set of subclasses that extend from this class.  You can observe this
+    array if you want to be notified when the object is extended.
+
+    @type SC.Set
+  */
+  subclasses: SC.Set.create(),
 
   /** @private */
   toString: function() { return SC._object_className(this); },
